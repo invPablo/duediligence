e1016c9f7412b84d8976cd646e1506456c06ab4c
Pablo <prodriguezrial@gmail.com>
2026-06-06 21:46:10 +0100

    fix: disable middleware protection temporarily

--- middleware.js
+++ middleware.js
@@ -1,17 +1,6 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
-const isProtectedRoute = createRouteMatcher([
-  '/stock(.*)',
-  '/screener(.*)',
-  '/compare(.*)',
-]);
-
-export default clerkMiddleware(async (auth, request) => {
-  if (isProtectedRoute(request)) {
-    const { userId, redirectToSignIn } = await auth();
-    if (!userId) return redirectToSignIn();
-  }
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: ['/((?!_next/static|_next/image|favicon.ico|.*\\.png$).*)'],
