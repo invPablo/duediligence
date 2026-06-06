f1bf7cd06e14463122204b71bff40c1887af7957
Pablo <prodriguezrial@gmail.com>
2026-06-06 21:47:25 +0100

    fix: restore middleware with correct matcher

--- middleware.js
+++ middleware.js
@@ -1,6 +1,16 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-export default clerkMiddleware();
+const isProtectedRoute = createRouteMatcher([
+  '/stock(.*)',
+  '/screener(.*)',
+  '/compare(.*)',
+]);
+
+export default clerkMiddleware(async (auth, request) => {
+  if (isProtectedRoute(request)) {
+    await auth.protect();
+  }
+});
 
 export const config = {
   matcher: ['/((?!_next/static|_next/image|favicon.ico|.*\\.png$).*)'],
