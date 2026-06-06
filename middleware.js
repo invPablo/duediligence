ae10b6f2989d20ac83beb720015a894c4c790b35
Pablo <prodriguezrial@gmail.com>
2026-06-06 22:54:48 +0100

    fix: use clerk account portal for auth

--- middleware.js
+++ middleware.js
@@ -3,8 +3,5 @@ import { clerkMiddleware } from '@clerk/nextjs/server';
 export default clerkMiddleware();
 
 export const config = {
-  matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.png$).*)',
-    '/__clerk/(.*)',
-  ],
+  matcher: ['/((?!_next/static|_next/image|favicon.ico|.*\\.png$).*)'],
 };
